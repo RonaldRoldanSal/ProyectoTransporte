0581c678cd7e8391b9af7984c8d626432eb094d6
SAMY <SAMY@SAMYMAN>
2016-10-13 17:07:59 -0500

    Mantenimiento de encomiendas - primer Avance

---
+++ View/encomiendas/encomiendas_registrar.php
@@ -0,0 +1,243 @@
+    <div class="col-md-12">
+        <h2><span class="glyphicon glyphicon-briefcase"></span> Registro de Encomiendas:</h2><hr>
+    </div>
+    <div class="col-md-12">
+        <form action="" method="POST">
+         <div class="col-md-12">
+         <div class="col-lg-6">
+            <fieldset class="well the-fieldset">
+                
+            <div class="col-md-10">
+        <div align="center">
+            <button type="submit" class="btn btn-primary"><span class="glyphicon glyphicon-search"></span> Buscar Remitente</button>
+        </div>
+    </div>    
+            
+                
+            <div class="col-md-12">
+                <label class="">Remitente:</label>
+                <div class="form-group">
+                    <input type="text" class="form-control" name="txtremitente" >
+                </div>
+            </div>
+            <div class="form-group" >   
+            <div class="col-md-6">
+                <label class="">Tipo Documento:</label>
+                <div class="form-group">
+                    <input type="text" class="form-control" name="txttipodoc" >
+                </div>
+            </div> 
+                </div>
+        
+            <div class="col-md-6">
+                <label class="">Número de Documento:</label>
+                <div class="form-group">
+                    <input type="text" class="form-control" name="txtapepat" >
+                </div>
+            </div>
+        
+            <div class="col-md-12">
+                <label class="">Direccion:</label>
+                <div class="form-group">
+                    <input type="text" class="form-control" name="txtapemat" >
+                </div>
+            </div>
+        
+            <div class="col-md-12">
+                <label class="">Telefono:</label>
+                <div class="form-group">
+                    <input type="text" class="form-control" name="txtdirecccion">
+                </div>
+            </div>
+        </fieldset>
+     
+     </div>
+    
+    <div class="col-lg-6">
+                <fieldset class="well the-fieldset">
+                    <div class="col-md-10">
+        <div align="center">
+            <button type="submit" class="btn btn-primary"><span class="glyphicon glyphicon-search"></span> Buscar Beneficiario</button>
+        </div>
+    </div>
+            <div class="col-md-12">
+                <label class="">Beneficiario:</label>
+                <div class="form-group">
+                    <input type="text" class="form-control" name="txtremitente" >
+                </div>
+            </div>
+                
+            <div class="col-md-6">
+                <label class="">Tipo Documento:</label>
+                <div class="form-group">
+                    <input type="text" class="form-control" name="txttipodoc" >
+                </div>
+            </div>    
+        
+             
+               
+        
+            <div class="col-md-6">
+                <label class="">Número de Documento:</label>
+                <div class="form-group">
+                    <input type="text" class="form-control" name="txtapepat" >
+                </div>
+            </div>
+        
+            <div class="col-md-12">
+                <label class="">Direccion:</label>
+                <div class="form-group">
+                    <input type="text" class="form-control" name="txtapemat" >
+                </div>
+            </div>
+        
+            <div class="col-md-12">
+                <label class="">Telefono:</label>
+                <div class="form-group">
+                    <input type="text" class="form-control" name="txtdirecccion">
+                </div>
+            </div>
+        </fieldset> 
+      
+     </div>
+   </div>
+        </form>
+    </div>
+   
+
+    <div class="col-md-12">
+    <fieldset class="well the-fieldset">
+        <div class="col-md-6">         
+            <div class="col-md-6">
+                <label class="">Codigo:</label>
+                <div class="form-group">
+                    <input type="text" class="form-control" name="txtremitente" >
+                </div>
+            </div>
+            <div class="col-md-6">
+                <label class="">    . </label>
+                <div class="form-group">
+                    <button type="submit" class="btn btn-primary"><span class="glyphicon glyphicon-search"></span> Buscar Ruta</button>
+                </div>
+            </div>
+               
+            <div class="col-md-6">
+                <label class="">Origen:</label>
+                <div class="form-group">
+                    <input type="text" class="form-control" name="txttipodoc" >
+                </div> 
+            </div>    
+        
+            <div class="col-md-6">
+                <label class="">Destino:</label>
+                <div class="form-group">
+                    <input type="text" class="form-control" name="txttipodoc" >
+                </div>
+            </div>
+        </div>
+            
+       
+        <div class="col-md-6">     
+        
+            <div class="col-md-6">
+                <label class="">Fecha:</label>
+                <div class="form-group">
+                    <input type="text" class="form-control" name="txttipodoc" >
+                </div>
+            </div> 
+            <div class="col-md-6">
+                <label class="">Codigo de Envio:</label>
+                <div class="form-group">
+                    <input type="text" class="form-control" name="txttipodoc" >
+                </div>
+            </div>    
+        
+            <div class="col-md-6">
+                <label class="">Monto Total:</label>
+                <div class="form-group">
+                    <input type="text" class="form-control" name="txttipodoc" >
+                </div>
+            </div>   
+        </div>       
+     
+     <div class="col-md-12"><hr></div>
+     
+           <div class="col-md-6">     
+        
+               <div class="col-md-12">
+                <label class="">Descripción:</label>
+                <div class="form-group">
+                    <input type="text" class="form-control" name="txttipodoc" >
+                </div>
+               </div> 
+               <div class="col-md-6">
+                <label class="">Cantidad:</label>
+                <div class="form-group">
+                    <input type="text" class="form-control" name="txttipodoc" >
+                </div>
+               </div>    
+        
+               <div class="col-md-6">
+                <label class="">Peso:</label>
+                  <div class="form-group">
+                    <input type="text" class="form-control" name="txttipodoc" >
+                  </div>
+                </div>  
+               <div class="col-md-12">
+                <label class="">Observación:</label>
+                  <div class="form-group">
+                    <input type="text" class="form-control" name="txttipodoc" >
+                  </div>
+                </div>   
+            </div> 
+        <div class="col-md-6">
+            <div class="col-md-6">
+                  <label class="">Tipo de Encomienda:</label>
+                   <div class="form-group">  
+                     <select class="form-control" id="optionuser" name="optionuser" >
+                      <option value="">Seleccione...</option>
+                      <option value="1">Caja</option>
+                      <option value="2">Sobre</option>
+                      <option value="3">Bolsa</option>
+                      <option value="4">Otros</option>
+                    </select>   
+                  </div>
+	    </div>
+            <div class="col-md-6">
+                <label class="">Precio:</label>
+                  <div class="form-group">
+                    <input type="text" class="form-control" name="txttipodoc" >
+                  </div>
+            </div>  
+           <div class="col-md-12"><br></div>
+           
+            <div class="col-md-4">
+                
+                    <button type="submit" class="btn btn-primary"><span class="glyphicon glyphicon-plus"></span> Agregar</button>
+                
+            </div>
+            
+            <div class="col-md-4">
+                
+                
+                    <button type="submit" class="btn btn-primary"><span class="glyphicon glyphicon-pencil"></span> Editar</button>
+                
+            </div>
+            
+            <div class="col-md-4">
+                 
+                    <button type="submit" class="btn btn-primary"><span class="glyphicon glyphicon-remove"></span> Eliminar</button>
+                
+            </div>
+        </div>    
+        </fieldset> 
+    </div>
+  
+
+<div class="col-md-12">
+        <div align="center">
+            <button type="submit" class="btn btn-primary"><span class="glyphicon glyphicon-floppy-disk"></span> Registrar Encomienda</button>
+        </div>
+    </div>
+
+
